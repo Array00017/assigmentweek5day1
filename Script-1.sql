3ef85f951c72bb0e2c985a6002c5c0aaa30b522e
Arnold <151367062+Array00017@users.noreply.github.com>
2024-01-06 09:58:52 -0500

    Add files via upload

---
+++ Script-1.sql
@@ -0,0 +1,64 @@
+
+
+
+
+SELECT *
+FROM actor f 
+where last_name LIKE 'H%';
+SELECT 'Wahlberg'
+
+
+
+SELECT *
+FROM payment p  f 
+where between 3.99 and 5.99;
+
+
+SELECT *
+FROM film f 
+WHERE title = '7'; 
+
+
+SELECT *
+FROM customer 
+WHERE first_name = 'willie'
+
+
+
+SELECT*
+from rental
+where employee 
+where title = .count(*)
+
+
+
+select * 
+from district
+where name = .count(*)
+
+
+select * 
+from film_actor
+where film_id = count(*) actors 
+
+
+select * 
+from store_id 1
+.count(*) last_name = 'es'
+
+
+select *
+from payment between 4.99 and 5.99
+where rentals sold = '250' with ids between '380' and '430'
+
+select* 
+from film 
+where category .count(*) and where rating .count = most 
+
+
+
+
+
+
+
+
